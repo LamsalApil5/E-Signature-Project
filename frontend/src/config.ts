b84c7a095ffb57c3e0029fbbff1006a5bfd62c30
LamsalApil5 <lamsalapil5@gmail.com>
2024-12-07 10:51:01 +0545

    errors solve

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 // Base URL of your API
 export const BASE_URL = 'https://yourapi.com/api';
 
-export const CHATGPT_KEY = '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'
+export const CHATGPT_KEY ="";
